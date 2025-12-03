7944ee219633d554a869f9ba79066dc8c40fea90
Justin Fallo <fallojustinp@gmail.com>
2025-12-03 11:03:21 +0100

    move helper functions to their own file

--- reproduce_tables/src/01_extract_tables.py
+++ reproduce_tables/src/01_extract_tables.py
@@ -1,56 +1,12 @@
 import os, time
-import fitz
 import anthropic
-from docling.document_converter import DocumentConverter, PdfFormatOption, InputFormat
-from docling.datamodel.pipeline_options import PdfPipelineOptions, TableStructureOptions, TableFormerMode
+
+from helper_functions import extract_pages_with_tables
 
 INPUT_PATH = './input/'
 INTERMEDIATE_PATH = './intermediate/'
 OUTPUT_PATH = './output/ground_truth/'
 
-# helper functions
-def get_tables_with_docling(pdf_path):
-    pdf_options = PdfPipelineOptions(
-        do_table_structure= True,
-        table_structure_options= TableStructureOptions(
-            do_cell_matching= True,
-            mode= TableFormerMode.ACCURATE
-        )
-    )
-    converter = DocumentConverter(
-        format_options = {
-            InputFormat.PDF: PdfFormatOption(
-                pipeline_options = pdf_options
-            )
-        }
-    )
-    result = converter.convert(source= pdf_path)
-
-    return result.document.tables
-
-def extract_pages_with_tables(paper_input_path, paper_output_path):
-    doc = fitz.open(paper_input_path)
-    pages_with_tables = []
-
-    for page in range(len(doc)):
-        single_page_pdf_path = os.path.join(INTERMEDIATE_PATH, 'tmp_page.pdf')
-        single_page = fitz.open()
-        single_page.insert_pdf(doc, from_page= page, to_page= page)
-        single_page.save(single_page_pdf_path)
-        single_page.close()
-
-        tables = get_tables_with_docling(single_page_pdf_path)
-        if len(tables) > 0:
-            pages_with_tables.append(page)
-
-    tables_pdf = fitz.open()
-
-    for page in pages_with_tables:
-        tables_pdf.insert_pdf(doc, from_page= page, to_page= page)
-
-    tables_pdf.save(paper_output_path)
-    tables_pdf.close()
-    doc.close()
 
 def extract_tables(paper, reproduction_list, paper_path, out_path):
     print(f'Writing tables for Paper {paper}.')
---
+++ reproduce_tables/src/helper_functions.py
@@ -0,0 +1,51 @@
+import fitz
+from docling.document_converter import DocumentConverter, PdfFormatOption, InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions, TableStructureOptions, TableFormerMode
+
+
+# extraction
+def get_tables_with_docling(pdf_path):
+    pdf_options = PdfPipelineOptions(
+        do_table_structure= True,
+        table_structure_options= TableStructureOptions(
+            do_cell_matching= True,
+            mode= TableFormerMode.ACCURATE
+        )
+    )
+    converter = DocumentConverter(
+        format_options = {
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_options = pdf_options
+            )
+        }
+    )
+    result = converter.convert(source= pdf_path)
+
+    return result.document.tables
+
+def extract_pages_with_tables(paper_input_path, paper_output_path):
+    doc = fitz.open(paper_input_path)
+    pages_with_tables = []
+
+    for page in range(len(doc)):
+        single_page_pdf_path = os.path.join(INTERMEDIATE_PATH, 'tmp_page.pdf')
+        single_page = fitz.open()
+        single_page.insert_pdf(doc, from_page= page, to_page= page)
+        single_page.save(single_page_pdf_path)
+        single_page.close()
+
+        tables = get_tables_with_docling(single_page_pdf_path)
+        if len(tables) > 0:
+            pages_with_tables.append(page)
+
+    tables_pdf = fitz.open()
+
+    for page in pages_with_tables:
+        tables_pdf.insert_pdf(doc, from_page= page, to_page= page)
+
+    tables_pdf.save(paper_output_path)
+    tables_pdf.close()
+    doc.close()
+
+
+# reproduction
